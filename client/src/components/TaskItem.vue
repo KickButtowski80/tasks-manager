0d7b47c6830ff4dc921a0388ba6c31a36079e912
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-23 17:17:08 -0800

    refactoring front end

---
+++ client/src/components/TaskItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="single-task" :class="{ 'task-completed': task.completed }">
+    <h5>
+      <span><i class="far fa-check-circle"></i></span>
+      {{ task.name }}
+    </h5>
+    <div class="task-links">
+      <router-link to="google.com" class="edit-link">
+        <i class="fas fa-edit"></i>
+      </router-link>
+      <!-- <a href="" class="edit-link">
+            <i class="fas fa-edit"></i>
+          </a> -->
+      <button type="button" class="delete-btn" @click="deleteTask()">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["task"],
+  data() {
+    return {};
+  },
+  methods: {
+    deleteTask() {
+      console.dir(this.task);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- client/src/components/TasksList.vue
+++ client/src/components/TasksList.vue
@@ -3,29 +3,18 @@
     <p class="loading-text">Loading...</p>
 
     <div class="tasks" v-for="task in tasks" :key="task.id">
-      <div class="single-task" :class="{ 'task-completed': task.completed }">
-        <h5>
-          <span><i class="far fa-check-circle"></i></span>
-          {{ task.name }}
-        </h5>
-        <div class="task-links">
-          <router-link to="google.com" class="edit-link">
-            <i class="fas fa-edit"></i>
-          </router-link>
-          <!-- <a href="" class="edit-link">
-            <i class="fas fa-edit"></i>
-          </a> -->
-          <button type="button" class="delete-btn" data-id="${taskID}">
-            <i class="fas fa-trash"></i>
-          </button>
-        </div>
-      </div>
+      <task-item :task="task"></task-item>
     </div>
   </section>
 </template>
 
 <script>
+import TaskItem from "./TaskItem.vue";
+
 export default {
+  components: {
+    TaskItem,
+  },
   data() {
     return {
       tasks: [],
