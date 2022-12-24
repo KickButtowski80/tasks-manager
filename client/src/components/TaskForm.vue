937c5da0e1d56f45333a57e51f8250bf6dc00c8b
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-23 18:08:21 -0800

    set up add task

--- client/src/components/TaskForm.vue
+++ client/src/components/TaskForm.vue
@@ -1,25 +1,40 @@
 <template>
-      <form class="task-form">
-      <h4>task manager</h4>
-      <div class="form-control">
-        <input
-          type="text"
-          name="name"
-          class="task-input"
-          placeholder="e.g. wash dishes"
-        />
-        <button type="submit" class="btn submit-btn">submit</button>
-      </div>
-      <div class="form-alert"></div>
-    </form>
+  <form class="task-form">
+    <h4>task manager</h4>
+    <div class="form-control">
+      <input
+        type="text"
+        name="name"
+        class="task-input"
+        placeholder="e.g. wash dishes"
+        v-model="taskName"
+      />
+      <button type="submit" class="btn submit-btn" @click.prevent="addTask">submit</button>
+    </div>
+    <div class="form-alert"></div>
+  </form>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      taskName: "",
+    };
+  },
+  methods: {
+    async addTask() {
+      await fetch("http://localhost:3000/api/v1/tasks/", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(this.taskName),
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
