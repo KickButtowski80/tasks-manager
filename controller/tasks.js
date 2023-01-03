3b422c62ff7bf8d87d3344e46f04f8f5566ea215
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-02 20:13:42 -0800

    sort tasks in reverse order

--- controller/tasks.js
+++ controller/tasks.js
@@ -2,7 +2,7 @@ const Task = require('../models/Task')
 
 const getAllTasks = async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        const tasks = await Task.find({}).sort({_id:-1})
         res.status(200).json({ tasks })
     } catch (error) {
         res.status(500).json({ msg: error.message })
