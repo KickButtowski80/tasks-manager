199fad5fa067f6afe2b66d6ed5460818f86b5b24
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-13 20:46:33 -0800

    added a comment

--- middleware/async.js
+++ middleware/async.js
@@ -3,6 +3,7 @@ const asyncWrapper = (fn) => {
     try {
       await fn(req, res, next)
     } catch (error) {
+      // send error to next middleware which is error-hanlder.js
       next(error)
     }
   }
